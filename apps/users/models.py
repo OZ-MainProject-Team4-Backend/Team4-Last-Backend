c103f9fc09076d36e0884678ae3d09928e15046d
moxopills <moxopills@gmail.com>
2025-11-03 18:54:19 +0900

    users 모델 생성 settings DB 연결 수정

--- apps/users/models.py
+++ apps/users/models.py
@@ -59,22 +59,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    # 충돌 해결용 related_name 추가
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name="groups",
-        blank=True,
-        related_name="custom_user_set",
-        related_query_name="custom_user",
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name="user permissions",
-        blank=True,
-        related_name="custom_user_permissions",
-        related_query_name="custom_user_perm",
-    )
-
     class Meta:
         db_table = "users"
         verbose_name = "User"
