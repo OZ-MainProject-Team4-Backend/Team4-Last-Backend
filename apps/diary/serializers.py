722306b41a14efff60e2959dbbb10a4af1fcb828
romiii35 <romiii9735@gmail.com>
2025-11-05 15:20:49 +0900

    fix : 다이어리 serializers 수정

--- apps/diary/serializers.py
+++ apps/diary/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-
-from ..weather.serializers import WeatherDataSerializer
+from apps.weather.serializers import ForecastQuerySerializer
 from .models import Diary
 
 
@@ -11,7 +10,7 @@ class DiaryListSerializer(serializers.ModelSerializer):  # 목록조회
 
 
 class DiaryDetailSerializer(serializers.ModelSerializer):  # 상세조회
-    weather_data = WeatherDataSerializer(read_only=True)
+    weather = ForecastQuerySerializer(source="weather_data",read_only=True)
 
     class Meta:
         model = Diary
@@ -22,11 +21,10 @@ class DiaryDetailSerializer(serializers.ModelSerializer):  # 상세조회
             "satisfaction",
             "notes",
             "image_url",
-            "weather_data",
+            "weather",
             "created_at",
             "updated_at",
         ]
-
 
 class DiaryCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,16 +35,14 @@ class DiaryCreateSerializer(serializers.ModelSerializer):
         user = self.context["request"].user
         return Diary.objects.create(user=user, **validated_data)
 
+class DiaryUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diary
+        fields = ["title", "satisfaction", "notes", "image_url", "weather_data"]  # date 제외
+
+
     def update(self, instance, validated_data):
-        for field in [
-            "date",
-            "title",
-            "satisfaction",
-            "notes",
-            "image_url",
-            "weather_data",
-        ]:
-            if field in validated_data:
-                setattr(instance, field, validated_data[field])
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
         instance.save()
         return instance
