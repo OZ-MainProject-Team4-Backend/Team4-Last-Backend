b542f20c4f835e3bb8ac1e080f97b22e73ce1278
kang9805 <zzionkk@naver.com>
2025-11-05 13:51:33 +0900

    feat: locations views & urls 구현 및 연결

---
+++ apps/locations/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import FavoriteLocationViewSet
+
+router = DefaultRouter()
+router.register(r"favorites", FavoriteLocationViewSet, basename="favorite-locations")
+
+urlpatterns = router.urls
