be920bab9faaee7dbc2171d69240ba0a9f3e402d
romiii35 <romiii9735@gmail.com>
2025-11-05 17:32:54 +0900

    app.urls 변경완료(맵핑추가 -diary)

--- apps/urls.py
+++ apps/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
     path("api/locations/", include("apps.locations.urls")),
     # weather
     path("api/weather/", include("apps.weather.urls")),
+    path("api/diary/", include("apps.diary.urls"), name="diary"),
 ]
