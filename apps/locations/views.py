39973b2830f204b8ce26c4a0f8c6190c7969b414
kang9805 <zzionkk@naver.com>
2025-11-06 17:17:23 +0900

    location 앱 views.py 수정 및 reorder 엔드포인트 구조 구현 추후 로직 추가

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -13,7 +13,7 @@ from .serializers import (
 class FavoriteLocationViewSet(viewsets.ModelViewSet):
     """
     즐겨찾기 지역 CRUD Viewset
-    기본 목록, 생성, 조회, 수정, 삭제 제공
+    기본 목록 생성, 조회, 수정, 삭제 제공
     SoftDeleteModel 기반 - deleted_at IS NULL 데이터만 조회
     """
 
@@ -32,18 +32,41 @@ class FavoriteLocationViewSet(viewsets.ModelViewSet):
         """
         DELETE /api/locations/favorites/{id}
         """
+        instance = self.get_object()
+        instance.delete()
+
+        favorites = FavoriteLocation.objects.filter(
+            user=request.user,
+            deleted_at__isnull=True
+        ).order_by("order")
+
+        for index, favorite in enumerate(favorites): # 중간에 구멍이 생겼을시 index(0,1,2) 기반으로 order 값 재할당
+            if favorite.order != index:
+                favorite.order = index
+                favorite.save(update_fields=["order"])
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
-    def alias_update(self):
+    def partial_update(self, request, *args, **kwargs):
         """
         PATCH /api/locations/favorites/{id}
+        별칭 변경
         """
-        pass
+        instance = self.get_object()
 
+        if "order" in request.data:
+            return Response(
+                {"detail": "order 변경은 /reorder 엔드포인트를 사용해야 합니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-    def reorder(self):
+        return super().partial_update(request, *args, **kwargs)
+
+    @action(detail=False, methods=["patch"], url_path="reorder")
+    def reorder(self, request):
         """
         PATCH /api/locations/favorites/reorder
+        이 로직은 다음 pr에서 구현 예정
         """
         pass
