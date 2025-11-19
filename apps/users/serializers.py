d053e97ab33b2bee2a321e75b77ac31eac791898
moxopills <moxopills@gmail.com>
2025-11-19 20:55:20 +0900

    마이페이지 연령대 선택 수정

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -216,6 +216,7 @@ class LoginResponseSerializer(serializers.Serializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     age_group = serializers.ChoiceField(
         choices=[
+            ("", ""),
             ("10", "ten"),
             ("20", "twenty"),
             ("30", "thirty"),
@@ -228,7 +229,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         allow_blank=True,
     )
     gender = serializers.ChoiceField(
-        choices=[("W", "여성"), ("M", "남성"), ("0", "기타")],
+        choices=[("", ""),
+                 ("W", "여성"),
+                 ("M", "남성"),
+                 ("0", "기타")
+                 ],
         required=False,
         allow_null=True,
         allow_blank=True,
